e845cbf815fb66dd2ee8c9edea6615e334e30a89
durlav <thedurlav@gmail.com>
2024-05-22 00:34:17 +0530

    final final commit

--- src/main/java/com/jsp/todo/contoller/ToDoController.java
+++ src/main/java/com/jsp/todo/contoller/ToDoController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@CrossOrigin(origins = {"${settings.cors_origin}"})
+@CrossOrigin
 @RequestMapping("/todo")
 public class ToDoController {
 	@Autowired
